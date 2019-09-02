2473475871366ac4ca1a3a7ab5c0dbbd76779bd8
Bao-Trung Nguyen <Bao-Trung.Nguyen@expaceo.com>
2019-09-02 12:29:08 +0200

    Init connect to Postgres

--- api/controllers/todoController.js
+++ api/controllers/todoController.js
@@ -1,6 +1,20 @@
 'use strict';
 const https = require('https');
+const {Client} = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl:true,
+});
+
+client.connect();
 
 exports.index = function(req, res){
-    res.send("Hello, this is page index from todo");
+    client.query('SELECT * FROM todo', (err, res) => {
+        if(err) throw err;
+        for(let row of res.row){
+            res.send(JSON.stringify(row));
+        }
+        client.end();
+    });   
 }
