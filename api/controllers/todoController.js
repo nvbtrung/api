3c0c97fdfb99c6679c687b810a9548009a325a9e
Bao-Trung Nguyen <Bao-Trung.Nguyen@expaceo.com>
2019-09-04 14:31:15 +0200

    Fix

--- api/controllers/todoController.js
+++ api/controllers/todoController.js
@@ -13,7 +13,7 @@ client.connect();
 //get
 exports.get = function(req, res){    
     client.query('SELECT * FROM todo;', (err, resd) => {
-        if(err) throw err;
+        if(err) {client.end();throw err;}
         let todos = [];
         for(let row of resd.rows){
             todos.push(new Todo(row.id, row.task_name, row.task_content, row.task_iscompleted));
@@ -25,20 +25,20 @@ exports.get = function(req, res){
 exports.delete = function(req, res){  
     let id = req.body.id;
     client.query('DELETE FROM todo WHERE Id = ' + id + ';', (err, resd) => {
-        if(err) throw err;
+        if(err) {client.end();throw err;}
         res.status(200).end();   
     });
 }
 //create
 exports.create = function(req, res){   
-    let taskName = req.body.taskName;
-    let taskContent = req.body.taskContent;
-    let isCompleted = req.body.isCompleted;
+    let taskName = req.body.TaskName;
+    let taskContent = req.body.TaskContent;
+    let isCompleted = req.body.IsCompleted;
     console.log(taskName + " , " + taskContent + "," + isCompleted);
     let cmd = "INSERT INTO todo(task_name, task_content, task_iscompleted) "
         + "VALUES('"+ taskName + "','" + taskContent + "'," + isCompleted +");";
     client.query(cmd, (err, resd) => {
-        if(err) throw err;
+        if(err) {client.end();throw err;}
         res.status(200);      
     });
 }
@@ -54,7 +54,7 @@ exports.update = function(req, res){
         + "task_iscompleted = " + isCompleted 
         + " where id = " + id + ";";
     client.query(cmd, (err, resd) => {
-        if(err) throw err;
+        if(err) {client.end();throw err;}
         res.status(200).end();
     });
 }
